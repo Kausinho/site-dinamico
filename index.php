2c98398ab41a4b71155f3488526da8e2b7e5d16a
Klaussio Brunow <kausinhobrunow@gmail.com>
2022-02-02 18:32:55 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras-chave do site"/>
+    <meta name="description" content="Descrição do meu web site"/>
+    <title>Projeto 01</title>
+</head>
+<body>
+    <header>
+        <div class="logo">Logomarca</div><!--logo-->
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav><!--desktop-->
+
+        <nav class="mobile">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav><!--desktop-->
+    </header>
+
+    <section class="banner-principal">
+        <form>
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="email" name="email" required />
+            <input type="submit" name="acao" value="Cadastrar!" />
+        </form>
+    </section><!--banner-principal-->
+
+    <section class="descricao-autor">
+        <div class="w50">
+            <h2>Klaussio B. Carvalho</h2>
+            <p>Ad non ea sit esse quis laborum sint duis nulla consectetur do duis. Reprehenderit fugiat ex aute minim labore enim do laboris quis elit sunt do. Anim eu consectetur ipsum et deserunt. Irure dolor exercitation fugiat laboris aute consequat incididunt cillum non. Aute ea aute fugiat laboris dolore esse amet adipisicing cillum cupidatat. Proident laborum eu enim pariatur sunt eu cillum cillum culpa.Magna sit et laborum non mollit dolor id. Fugiat proident eiusmod sunt deserunt fugiat id quis ad reprehenderit. Voluptate magna duis fugiat labore ex dolor aute. Non ea ad consectetur adipisicing ex nostrud ex amet. Commodo sunt cupidatat reprehenderit commodo ea tempor in occaecat in pariatur est.</p>
+            <p>Ad non ea sit esse quis laborum sint duis nulla consectetur do duis. Reprehenderit fugiat ex aute minim labore enim do laboris quis elit sunt do. Anim eu consectetur ipsum et deserunt. Irure dolor exercitation fugiat laboris aute consequat incididunt cillum non. Aute ea aute fugiat laboris dolore esse amet adipisicing cillum cupidatat. Proident laborum eu enim pariatur sunt eu cillum cillum culpa.Magna sit et laborum non mollit dolor id. Fugiat proident eiusmod sunt deserunt fugiat id quis ad reprehenderit. Voluptate magna duis fugiat labore ex dolor aute. Non ea ad consectetur adipisicing ex nostrud ex amet. Commodo sunt cupidatat reprehenderit commodo ea tempor in occaecat in pariatur est.</p>
+        </div><!--w50-->
+        <div class="w50">
+            <img src="images/avatar.jpg" />
+        </div><!--w50-->
+    </section><!--descricao-autor-->
+</body>
+</html>

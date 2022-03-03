11cb6ab8a23e91c7ed9d28819f6de24424ce21e8
Klaussio Brunow <kausinhobrunow@gmail.com>
2022-03-03 09:58:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -39,14 +39,23 @@
     </section><!--banner-principal-->
 
     <section class="descricao-autor">
-        <div class="w50">
-            <h2>Klaussio B. Carvalho</h2>
-            <p>Ad non ea sit esse quis laborum sint duis nulla consectetur do duis. Reprehenderit fugiat ex aute minim labore enim do laboris quis elit sunt do. Anim eu consectetur ipsum et deserunt. Irure dolor exercitation fugiat laboris aute consequat incididunt cillum non. Aute ea aute fugiat laboris dolore esse amet adipisicing cillum cupidatat. Proident laborum eu enim pariatur sunt eu cillum cillum culpa.Magna sit et laborum non mollit dolor id. Fugiat proident eiusmod sunt deserunt fugiat id quis ad reprehenderit. Voluptate magna duis fugiat labore ex dolor aute. Non ea ad consectetur adipisicing ex nostrud ex amet. Commodo sunt cupidatat reprehenderit commodo ea tempor in occaecat in pariatur est.</p>
-            <p>Ad non ea sit esse quis laborum sint duis nulla consectetur do duis. Reprehenderit fugiat ex aute minim labore enim do laboris quis elit sunt do. Anim eu consectetur ipsum et deserunt. Irure dolor exercitation fugiat laboris aute consequat incididunt cillum non. Aute ea aute fugiat laboris dolore esse amet adipisicing cillum cupidatat. Proident laborum eu enim pariatur sunt eu cillum cillum culpa.Magna sit et laborum non mollit dolor id. Fugiat proident eiusmod sunt deserunt fugiat id quis ad reprehenderit. Voluptate magna duis fugiat labore ex dolor aute. Non ea ad consectetur adipisicing ex nostrud ex amet. Commodo sunt cupidatat reprehenderit commodo ea tempor in occaecat in pariatur est.</p>
-        </div><!--w50-->
-        <div class="w50">
-            <img src="images/avatar.jpg" />
-        </div><!--w50-->
+        <div class="center">
+            <div class="w50">
+                <h2>Klaussio B. Carvalho</h2>
+                <p>Ad non ea sit esse quis laborum sint duis nulla consectetur do duis. Reprehenderit fugiat ex aute minim labore enim do laboris quis elit sunt do. Anim eu consectetur ipsum et deserunt. Irure dolor exercitation fugiat laboris aute consequat incididunt cillum non. Aute ea aute fugiat laboris dolore esse amet adipisicing cillum cupidatat. Proident laborum eu enim pariatur sunt eu cillum cillum culpa.Magna sit et laborum non mollit dolor id. Fugiat proident eiusmod sunt deserunt fugiat id quis ad reprehenderit. Voluptate magna duis fugiat labore ex dolor aute. Non ea ad consectetur adipisicing ex nostrud ex amet. Commodo sunt cupidatat reprehenderit commodo ea tempor in occaecat in pariatur est.</p>
+                <p>Ad non ea sit esse quis laborum sint duis nulla consectetur do duis. Reprehenderit fugiat ex aute minim labore enim do laboris quis elit sunt do. Anim eu consectetur ipsum et deserunt. Irure dolor exercitation fugiat laboris aute consequat incididunt cillum non. Aute ea aute fugiat laboris dolore esse amet adipisicing cillum cupidatat. Proident laborum eu enim pariatur sunt eu cillum cillum culpa.Magna sit et laborum non mollit dolor id. Fugiat proident eiusmod sunt deserunt fugiat id quis ad reprehenderit. Voluptate magna duis fugiat labore ex dolor aute. Non ea ad consectetur adipisicing ex nostrud ex amet. Commodo sunt cupidatat reprehenderit commodo ea tempor in occaecat in pariatur est.</p>
+            </div><!--w50-->
+            <div class="w50">
+                <img src="images/avatar.jpg" />
+            </div><!--w50-->
+        </div><!--center-->
     </section><!--descricao-autor-->
+
+    <section class="especialidades">
+        <h2 class="title">Especialidades</h2><!--title-->
+        <div class="center">
+
+        </div><!--center
+    </section><!--especialidades-->
 </body>
 </html>

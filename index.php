d177aa2dad80a5f878df0d5d1348c3751d37fe6d
Klaussio Brunow <kausinhobrunow@gmail.com>
2022-03-06 05:00:13 -0300

    link icons

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 
     <!--LINKS DO SITE-->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Righteous&family=Sarala:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="style/style.css" type="text/css" rel="stylesheet" />
 
 </head>
